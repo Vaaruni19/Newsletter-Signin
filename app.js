cd6a18560ef4bf4e0598f9c9a476a646ed64432f
Vaaruni Choubey <104271274+Vaaruni19@users.noreply.github.com>
2023-03-18 11:55:02 +0530

    JavaScript files
    These files do the work at the backend.

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const https = require("https");
+
+app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+
+app.post("/", function(req,res){
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
+    const data = {
+        members: [
+            {
+              email_address: email,
+              status: 'subscribed',
+              merge_fields: {
+                  FNAME: firstName,
+                  LNAME: lastName
+              }
+            }
+          ]
+    }
+
+    const jsonData = JSON.stringify(data);
+    const url = "https://us11.api.mailchimp.com/3.0/lists/6989cd3ec8";
+    const options = {
+        method: "POST",
+        auth: "vaaruni:5866b10a30ded2c2aa2a8c596b50b33f-us11"
+
+    };
+
+    const request = https.request(url, options, function(response){
+        if(response.statusCode===200){
+            res.sendFile(__dirname + "/success.html");
+        }
+        else{
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+    });
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
+});
+
+
+
+app.listen(3000,function(){
+    console.log("Server Started!");
+});
+
+
